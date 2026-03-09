12be3361dffc3c446c88cf28c015482a81513925
hu0528hu <2645602177@qq.com>
2026-03-09 21:29:05 +0800

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+   
+        ERyder bike1=new ERyder();
+        bike1.printBikeDetails();
+
+        ERyder bike2=new ERyder("112233",1,true,1.0);
+        bike2.printBikeDetails();
+    }
+}
+
